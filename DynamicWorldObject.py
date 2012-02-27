839707a5b6980d64fd7100750be98a2ce7c203bf
MattWis <matthew.wismer@students.olin.edu>
2012-02-26 22:30:48 -0500

    Added docstring

--- DynamicWorldObject.py
+++ DynamicWorldObject.py
@@ -6,8 +6,7 @@ import math
 
 class DyanmicWorldObject(WorldObject):
 
-"""Class 
-
+"""Class DynamicWorldObject: This is a child of WorldObject that can move. This object must have a positive mass, and can be moved by BulletWorld.
 
 """
 	def __init__(self, world, attach, name = '', xCoord = 0, yCoord = 0, zCoord = 0, shape = BulletBoxShape(Vec3(.1, .1, .1)), heading = 0, pitch = 0, roll = 0, xVel = 0, yVel = 0, zVel = 0, mass = 0.1):
