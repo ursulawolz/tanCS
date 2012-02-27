548114ac61d8a6e8137f885d744ab4dac5ff9f1f
MattWis <matthew.wismer@students.olin.edu>
2012-02-26 22:18:37 -0500

    Reformatting Inheritances

---
+++ DynamicWorldObject.py
@@ -0,0 +1,18 @@
+from panda3d.core import Vec3, Point3, BitMask32
+from pandac.PandaModules import PandaNode
+from direct.showbase.ShowBase import ShowBase
+from panda3d.bullet import BulletRigidBodyNode, BulletBoxShape, BulletWorld, BulletCapsuleShape
+import math
+
+class DyanmicWorldObject(WorldObject):
+
+"""Class 
+
+
+"""
+	def __init__(self, world, attach, name = '', xCoord = 0, yCoord = 0, zCoord = 0, shape = BulletBoxShape(Vec3(.1, .1, .1)), heading = 0, pitch = 0, roll = 0, xVel = 0, yVel = 0, zVel = 0, mass = 0.1):
+		
+		if (mass <=0):
+			raise ValueError("Mass of DynamicWorldObject must be greater than 0.")
+	
+		WorldObject.__init__(self, world, attach, name, xCoord, yCoord, zCoord, shape, heading, pitch, roll, xVel, yVel, zVel, mass)
