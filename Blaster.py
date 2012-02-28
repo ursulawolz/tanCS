73b372a9b5cee304c0a0fd3194699d7bb7cb0519
luke metz <luke.s.metz@gmail.com>
2012-02-28 10:16:42 -0800

    added blaster weopon

---
+++ Blaster.py
@@ -0,0 +1,15 @@
+from Weapon import *
+
+
+class Blaster(Weapon):
+	'''
+	A blaster class
+	'''
+	def __init__(self):
+		Weapon.__init__()
+
+	def fire():
+		'''
+		Fires a blaster bullet
+		'''
+		print "fire the bullet here"
