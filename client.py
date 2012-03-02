2e932fda7b02733b816df96b71254f68866f6939
Reilly Butler <reilly@students.olin.edu>
2012-03-02 16:57:29 -0500

    SSL networking test. Closes #26.

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import socket
 import sys
+import ssl
 
 if len(sys.argv) != 2:
     sys.exit()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((sys.argv[1], 31415))
+ss = ssl.wrap_socket(s)
+ss.connect((sys.argv[1], 31415))
 
 question = 'Are you a little teapot?'
 print('<', question)
-s.send(bytes(question, 'utf-8'))
-answer = str(s.recv(4096),'utf-8')
+ss.send(bytes(question, 'utf-8'))
+answer = str(ss.recv(4096),'utf-8')
 print('>', answer)
-s.close()
+ss.close()
--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import socket
 import sys
+import ssl
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 31415))
 s.listen(1)
-c, a = s.accept()
-s.shutdown(socket.SHUT_RDWR)
-s.close()
+ss = ssl.wrap_socket(s, certfile='cert.pem', server_side=True)
+c, a = ss.accept()
+ss.close()
 
 question = str(c.recv(4096),'utf-8')
 print('>', question)
