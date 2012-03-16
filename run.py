f195c83ea703cc539491e5eceda5665637e1e471
Luke Metz <luke.metz@students.olin.edu>
2012-03-16 12:19:23 -0400

    added level loader in run.py

--- run.py
+++ run.py
@@ -8,30 +8,29 @@ import TaskList
 from panda3d.bullet import BulletPlaneShape
 from panda3d.bullet import BulletRigidBodyNode
 from runFile import runFile
+
+import xmlParse
+
 print("asdf")
-t = TankWorld()
-t.getPhysics().setGravity(Vec3(0,0,-9.81))
 
 
-static = MeshWorldObject(t,render, 'rock1.egg', position=Vec3(0,10,0))
-static = MeshWorldObject(t,render, 'tree.egg', position=Vec3(4,4,4))
+t = xmlParse.createLevel('testLevel.xml')
 
 shape = BulletPlaneShape(Vec3(0, 0, 1), 1)
 
 node = BulletRigidBodyNode('Ground')
-node.setFriction(1);
+node.setFriction(1)
 node.addShape(shape)
 
 np = render.attachNewNode(node)
-np.setPos(0, 0, -4)
-
+np.setPos(0,0,-4)
 t.getPhysics().attachRigidBody(node)
 
-print t.getPhysics().getNumRigidBodies(), "before"
-#dynamic = DynamicWorldObject(t.getPhysics(),render)
+print "asdfasdfasdf"
+#np.setPos(0, 0, 0)
+
 
 runFile('user.py',t)
-print t.getPhysics().getNumRigidBodies(), "after"
 
 #TaskList.setCollision(ta, t)
 
