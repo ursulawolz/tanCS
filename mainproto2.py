3b8af94dc20b87ef5e1c3d44ed40089bdc1c03bc
code-11 <brendan.ritter@yahoo.com>
2012-04-03 15:06:08 -0400

    Added the main file for all three gui classes. It can switch between them. I then added the new viewer class.

---
+++ mainproto2.py
@@ -0,0 +1,43 @@
+###------------------------------Inclusions--------------------------------###
+from gi.repository import Gtk,GObject
+from gi.repository import Gdk
+import viewerproto2 as viewer
+###-------------------------------Menu UI----------------------------------###
+UI_INFO="""
+<ui>
+	<menubar name='MenuBar'>
+		<menu action='ModeMenu'>
+			<menuitem action='Editor'/>
+			<separator />
+			<menuitem action='Viewer'/>
+			<separator />
+			<menuitem action='Explorer'/>
+		</menu>
+	</menubar>
+</ui>
+				
+"""
+###-------------------------------MAIN SWITCH------------------------------###
+def on_menu_mode_changed(widget, current,parent_window):
+	new_window_name=current.get_name()
+	if new_window_name==("Viewer"):
+		window=viewer.TempWindow(UI_INFO,on_menu_mode_changed)
+		window.connect("delete-event",Gtk.main_quit)
+		window.show_all()
+	if new_window_name==("Explorer"):
+		window=viewer.TempWindow(UI_INFO,on_menu_mode_changed)
+		window.connect("delete-event",Gtk.main_quit)
+		window.show_all()
+	if new_window_name==("Editor"):
+		window=viewer.TempWindow(UI_INFO,on_menu_mode_changed)
+		window.connect("delete-event",Gtk.main_quit)
+		window.show_all()
+	parent_window.destroy()
+	
+	#print current.get_name() + " was selected."
+###-----------------------------------MAIN---------------------------------###
+
+win=viewer.TempWindow(UI_INFO,on_menu_mode_changed)
+win.connect("delete-event",Gtk.main_quit)
+win.show_all()
+Gtk.main()	
