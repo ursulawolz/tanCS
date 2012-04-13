73c7702976d6dd92b0ae216ba94cba512bc5a3fd
Jazmin Gonzalez-Rivero <jaz.gonzaro@gmail.com>
2012-04-13 13:48:31 -0400

    IT WORKS!!!! no bugs....if you are trying to add/update/search/call any thing on an account....as for all other things.....having been tested yet but should work

--- makeDatabase.py
+++ makeDatabase.py
@@ -11,6 +11,8 @@ def makeTableStructure(host , password , username, personalpassword) :
 
 	cur.execute("create database oogadaBoogada")
 	cur.execute("use oogadaBoogada")
+	cur.execute("grant usage on *.* to " + username + " identified by '" + personalpassword + "' ;")
+	cur.execute("grant all privileges on oogadaBoogada.* to " + username + ";")
 	cur.execute("create table account(account_id int, username tinytext, password tinytext, avatar blob, groups text);")
 	cur.execute("create table borrow( borrow_id int, date_taken datetime, project_to_id smallint, project_id smallint, revision_id smallint, file_name tinytext, line_range tinytext);")
 	cur.execute("create table comment(all_of_the_text longtext, time_of_post datetime, account_posting_id smallint, project_id smallint, revision_number smallint, file_name tinytext, line_number tinyint);")
@@ -21,13 +23,12 @@ def makeTableStructure(host , password , username, personalpassword) :
 	
 
 
-	cur.execute("grant usage on *.* to " + username + " identified by" + personalpassword + ";")
-	cur.execute("grant all privileges on oogadaBoogada.* to " + username + ";")
+	
 
 
 
 
-def update(information , information_type , identifier , identifier_type):
+def Update(information , information_type , identifier , identifier_type, host,password):
 
 	import MySQLdb as mdb 
 	import sys
@@ -35,79 +36,140 @@ def update(information , information_type , identifier , identifier_type):
 	con = None
 	con = mdb.connect(host, 'root', password)
 	cur = con.cursor()
+	cur.execute("use oogadaBoogada")
+	if type(information) == str:
+		information = "'" + information + "'"
+	else:
+		information = str(information)
+	if type(identifier) == str:
+		identifier = "'" + identifier + "'"
+	else:
+		identifier= str(identifier)
 
 	if information_type == "file_content" :
-		cur.execute("update file_info set %s =  %s  where  %s = %s" ,(information_type, information , identifier_type , identifier))
+		cur.execute("update file_info set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
 
 	elif information_type == "filenames":
-		cur.execute("update revision set %s =  %s  where  %s = %s" ,(information_type, information , identifier_type , identifier))
+		cur.execute("update revision set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
 
 
-	elif information_type == "avatar" || information_type == "password" :
-		cur.execute("update account set %s =  %s  where  %s = %s" ,(information_type, information , identifier_type , identifier))
+	elif information_type == "avatar" or information_type == "password" :
+		cur.execute('update account set ' + information_type + ' = ' + information + ' where ' + identifier_type + ' = ' + identifier)
 
 	elif information_type == "locked" :
-		cur.execute("update project_info set %s =  %s  where  %s = %s" ,(information_type, information , identifier_type , identifier))
+		cur.execute("update project_info set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
 
 
-	elif information_type == "tags" || information_type == "children_ids" || information_type == "borrow_ids" || information_type == "revisions" :
+	elif information_type == "tags" or information_type == "children_ids" or information_type == "borrow_ids" or information_type == "revisions" :
 		con.query("select %s from project_info where %s = %s" , (information_type , identifier_type, identifier))
 		current = con.store_result()
 		new_info = current + information
-		cur.execute("update project_info set %s =  %s  where  %s = %s" ,(information_type, new_info, identifier_type , identifier))
+		cur.execute("update project_info set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
 
 
 	elif information_type == "group_projects":
 		con.query("select %s from group_info where %s = %s" , (information_type , identifier_type, identifier))
 		current = con.store_result()
 		new_info = current + information
-		cur.execute("update group_info set %s =  %s  where  %s = %s" ,(information_type, new_info, identifier_type , identifier))
+		cur.execute("update group_info set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
 
 	elif information_type == "groups" :
 		con.query("select %s from account where %s = %s" , (information_type , identifier_type, identifier))
 		current = con.store_result()
 		new_info = current + information
 		cur.execute("update account set %s =  %s  where  %s = %s" ,(information_type, new_info , identifier_type , identifier))
-		cur.execute("update group_info set %s =  %s  where  %s = %s" ,(identifier_type , identifier, information_type, information))
+		cur.execute("update group_info set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
 
 	elif information_type == "account_ids":
 		con.query("select %s from group_info where %s = %s" , (information_type , identifier_type, identifier))
 		current = con.store_result()
 		new_info = current + information
-		cur.execute("update group_info set %s =  %s  where  %s = %s" ,(information_type, new_info , identifier_type , identifier))
-		cur.execute("update account set %s =  %s  where  %s = %s" ,(identifier_type , identifier, information_type, information))
+		cur.execute("update group_info set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
+		cur.execute("update account set " + information_type + " = " + information + " where " + identifier_type + " = " + identifier)
+
+	con.commit()
 			
 
 
 
 
-def NewEntry(object_type, column_info, column_type):
-	for i in range column_info:
-		cur.execute("insert into %s ( %s ) values %s", (object_type, colomn_type[i], column_info[i]))
+def NewEntry(object_type, column_info, column_type, host,password):
+	import MySQLdb as mdb 
+	import sys
+	
+	con = None
+	con = mdb.connect(host, 'root', password)
+	cur = con.cursor()
+	cur.execute("use oogadaBoogada")
+
+	infos = ''
+	types = ''
+	for i in range(len(column_info)):
+		if not type(column_info[i]) == str:
+			column_info[i] = str(column_info[i])
+		if i == len(column_info) - 1: 
+			types = types + column_type[i]
+			infos = infos + "'" + column_info[i] + "'"
+		else:
+			infos = infos + "'" + column_info[i] + "' ,"
+			types = types + column_type[i] + ',' 
 
 
-	if object_type = "borrow":
-		update(column_info[0], "borrow_ids", column_info[3], "project_id" )
-
-	elif object_type = "project_info":
-		update(column_info[0], "group_projects", column_info[4], "group_id" )
-
-	elif object_type = "group_info":
-		update(column_info[0], "groups", column_info[3], "account_id" )
-
-	elif object_type = "file_info":
-		update(column_info[2], "filenames", column_info[1], "revision_number" )
-
-	elif object_type = "revision":
-		update(column_info[0], "revisions", column_info[2], "project_id" )
+	cur.execute('insert into ' + object_type + '(' + types + ')' + 'values (' + infos + ');')
+	con.commit()
 
 
-def SearchAll(search_type, search_value, object_type, fist, last):  # ex: give me all of the object_type who have search_type equal to search_value
+	if object_type == "borrow":
+		update(column_info[0], "borrow_ids", column_info[3], "project_id", host,password )
+
+	elif object_type == "project_info":
+		update(column_info[0], "group_projects", column_info[4], "group_id" , host,password)
+
+	elif object_type == "group_info":
+		update(column_info[0], "groups", column_info[3], "account_id", host,password )
+
+	elif object_type == "file_info":
+		update(column_info[2], "filenames", column_info[1], "revision_number" , host,password)
+
+	elif object_type == "revision":
+		update(column_info[0], "revisions", column_info[2], "project_id" , host,password)
+ 
+
+def SearchAll(search_type, search_value, object_type, fist, last, host, password):  # ex: give me all of the object_type who have search_type equal to search_value
+	
+	import MySQLdb as mdb 
+	import sys
+	
+	con = None
+	con = mdb.connect(host, 'root', password)
+	cur = con.cursor()
+	cur.execute("use oogadaBoogada")
+
 	string_of_stuff = ""
-	for i in range search_types:
-		string_of_stuff = string_of_stuff + 'or' + search_type[i] + 'like "%'  + search_value[i] + '%"'
+	for i in range(len(search_value)):
 
-	con.query("select * from %s where %s limit %s,%s" , (object_type, string_of_stuff, fist, last))
+			if not type(search_value[i]) == str:
+				search_value[i] = str(search_value[i])
+			if i == len(search_value) - 1:
+				string_of_stuff = string_of_stuff + search_type[i] + ' like "%'  + search_value[i] + '%"'
+			else:
+				string_of_stuff = string_of_stuff + search_type[i] + ' like "%'  + search_value[i] + '%" or '
+	
+	con.query(" select * from " + object_type + " where " + string_of_stuff + ' limit ' + fist+ ' ,' + last)
 	stuff = con.store_result() 
 	return stuff
 
+
+
+
+
+
+
+
+# test
+makeTableStructure('localhost', 'olin' , 'jrivero', 'x8mafw63')
+meep = [123, 'FearBisquick', 'weee', 'NULL', 'groups and things']
+moop = ['account_id', 'username' , 'password', 'avatar', 'groups' ]
+NewEntry('account', meep, moop, 'localhost', 'olin')
+SearchAll(['groups', 'account_id'] , ['and', 12], 'account', '0' , '1', 'localhost' , 'olin')
+Update('weeeee', 'password' , 123 , 'account_id', 'localhost', 'olin')
