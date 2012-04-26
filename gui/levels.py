55971dfce20730fafb938e68760c2eee28e31480
lukemetz <luke.metz@students.olin.edu>
2012-04-26 12:40:51 -0400

    added gui run function, fixed up gui alittle.

--- gui/levels.py
+++ gui/levels.py
@@ -34,8 +34,10 @@ class GridWindow(Gtk.Window):
 		try:
 			f = open('saved.data')
 			self.worldNum = int(f.readline().strip().split('orld')[1])
+
 			self.levelNum = int(f.readline().strip().split('evel')[1])
 		except:
+			print "Could not read"
 			self.worldNum = 1
 			self.levelNum = 1
 
@@ -83,7 +85,7 @@ class GridWindow(Gtk.Window):
 		grid.attach(self.levelGrid,1,0,1,2)
 		#self.boxSeperator.add(self.levelGrid)
 		self.levelGrid.set_border_width(15)
-		self.worldNameLabel=
+		#self.worldNameLabel=
 		self.levelNameLabel=Gtk.Label("No level Selected")
 		self.levelNameLabel.set_alignment(0,1)
 		self.levelGrid.attach(self.levelNameLabel, 0, 1, 0, 1)
@@ -96,15 +98,15 @@ class GridWindow(Gtk.Window):
 		self.levelDescription=Gtk.Label("No level Descriptions lololol")
 		#self.levelGrid.attach_next_to( self.levelDescription, self.levelImage,Gtk.PositionType.BOTTOM,2,2)
 		self.levelGrid.attach( self.levelDescription, 0, 1, 2, 3)
-	
+		print self.levelNum
 		self.selectLevel(None, levels[self.worldNum-1]['levelInfo'][self.levelNum-1], self.worldNum, self.levelNum)
 
-	def __del__(self):
+	def close(self, unused1, unused2):
 		f = open('saved.data', 'w')
-		f.write('World'+str(self.worldNum)+'\n'+'Level'+str(self.worldNum)+'\n')
+		f.write('World'+str(self.worldNum)+'\n'+'Level'+str(self.levelNum)+'\n')
 		f.close()
-
-	def selectLevel(self, button, level, levelnum, worldnum):
+		print "Closed Gui"
+	def selectLevel(self, button, level,worldnum,levelnum):
 		self.levelNameLabel.set_text(level[0] + ": "+ level[1])
 		self.levelDescription.set_text(level[2])
 		self.levelImage.set_from_file('level1.png')
@@ -115,6 +117,7 @@ class GridWindow(Gtk.Window):
 
 win=GridWindow()
 win.connect("delete-event", Gtk.main_quit)
+win.connect("delete-event", win.close)
 win.show_all()
 Gtk.main()
 
